be5930d0709f690327d443160142160b7779e870
Stefan Frede <stefan@frede.dev>
2020-09-22 20:09:40 +0200

    fix: prevent webpack from tree shaking alpine (#12)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,11 @@
 import confetti from 'canvas-confetti';
 
 // eslint-disable-next-line
-import 'alpinejs';
+import alpinejs from 'alpinejs';
+
+// Quick & dirty way to prevent webpack from tree shaking Alpine.js
+const body = document.querySelector('body');
+body.setAttribute('data-alpinejs', alpinejs.version);
 
 const canvas = document.getElementById('canvas');
 
