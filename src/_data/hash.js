2d31622de74b9ca3c10d92fd49166190fde896db
Stefan Frede <stefan@frede.dev>
2020-09-24 12:34:05 +0200

    feat: improve font loading (#16)

---
+++ src/_data/hash.js
@@ -0,0 +1,7 @@
+module.exports = function () {
+  const segment = () => {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  };
+
+  return `${segment()}-${segment()}-${segment()}`;
+};
