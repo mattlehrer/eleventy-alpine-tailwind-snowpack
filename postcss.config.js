3935f2fc91afcc85f6b8f879462ccbd9d6b5b46e
Stefan Frede <stefan@frede.dev>
2020-09-30 15:43:48 +0200

    feat: add postcss 100vh fix

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
+    require('postcss-100vh-fix'),
     require('postcss-preset-env')({ stage: 3 }),
     require('cssnano')({
       preset: 'default',
