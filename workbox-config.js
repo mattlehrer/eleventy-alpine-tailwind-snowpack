baf38214a84eae7064961bc0472ff0302e3c630c
Matt Lehrer <lehrerm@gmail.com>
2021-01-30 23:50:32 -0800

    fix: remove inter font references

--- workbox-config.js
+++ workbox-config.js
@@ -16,27 +16,6 @@ module.exports = {
       },
     },
     {
-      urlPattern: /^(https:\/\/rsms\.me\/inter\/).+(.css)$/,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'inter-font-stylesheet',
-      },
-    },
-    {
-      urlPattern: /^(https:\/\/rsms\.me\/inter\/font-files\/)/,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'inter-font-webfont',
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-        expiration: {
-          maxAgeSeconds: 60 * 60 * 24 * 365,
-          maxEntries: 30,
-        },
-      },
-    },
-    {
       urlPattern: /\.(?:png|jpg|jpeg|gif|webp|svg|ico)$/,
       handler: 'CacheFirst',
       options: {
