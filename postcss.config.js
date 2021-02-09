ab4122e5c114bb0cd377be6e90c2649cf44725ec
Matt Lehrer <lehrerm@gmail.com>
2021-02-08 16:09:30 -0800

    feat: setup postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,15 @@
+const postcssPresetEnv = require('postcss-preset-env');
+
 module.exports = {
-  plugins: [require('tailwindcss'), require('autoprefixer')],
+  plugins: [
+    postcssPresetEnv({
+      /* use stage 3 features + css nesting rules */
+      // stage: 3,
+      features: {
+        'nesting-rules': true,
+      },
+    }),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 };
